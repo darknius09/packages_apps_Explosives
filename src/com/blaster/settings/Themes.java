40be97d57b94da62e9bdb105a6322a6f9496a398
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-06-10 10:40:51 +0530

    Explosives: Nuke accent overlays

--- src/com/blaster/settings/Themes.java
+++ src/com/blaster/settings/Themes.java
@@ -74,8 +74,6 @@ public class Themes extends DashboardFragment {
             Context context, Lifecycle lifecycle, Fragment fragment) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.accent_color"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
